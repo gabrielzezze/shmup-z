9bc42a6c5a787611310746d16eee1fefaa2e62c5
root <root@DESKTOP-ST49TAF.localdomain>
2021-03-05 15:30:56 -0300

    WIP Almost done second tutortial

---
+++ Assets/_Scripts/ShotBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotBehaviour : SteerableBehaviour {
+
+    private void Update() {
+        Thrust(1, 0);
+    }
+
+}
